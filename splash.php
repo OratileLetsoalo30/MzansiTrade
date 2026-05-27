73077424d0e8e9225a51f1b3c288b5e397219891
OratileLetsoalo30 <oratile.letsoalo@icloud.com>
2026-05-27 20:07:37 +0200

    added project pages

---
+++ splash.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MzansiTrade</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Redirect after 4 seconds -->
+    <meta http-equiv="refresh" content="4;url=index.php">
+</head>
+
+<body class="splash-body">
+
+    <div class="splash-container">
+
+        <img src="assets/img/logo.jpeg" alt="MzansiTrade Logo" class="splash-logo">
+
+        <h1 class="splash-title">MzansiTrade</h1>
+
+        <p class="splash-text">
+            Cape Town’s Local Marketplace
+        </p>
+
+        <div class="loader"></div>
+
+    </div>
+
+</body>
+</html>
